c0674d1e2898de820bb09431886653070ce5c96e
thanh2004nguyen <116701025+thanh2004nguyen@users.noreply.github.com>
2023-07-05 13:48:29 +0700

    new

--- src/component/post/index.tsx
+++ src/component/post/index.tsx
@@ -5,7 +5,33 @@ const PostItem=()=>{
 
     const cx=classNames.bind(Style)
 
-return(<div className={cx("wrapper")}>post</div> )
+    return(
+        <div className={cx("wrapper")}>
+
+            <div className={cx("cart")}>
+                <div className={cx("info")}>
+                    <img src="https://images.unsplash.com/photo-1449426468159-d96dbf08f19f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW90b3JiaWtlfGVufDB8fDB8fHww&w=1000&q=80" alt="bike" />
+                    <div className={cx("name")}><p>name</p><span>info</span></div>
+                    <p className={cx("icon")}>...</p>
+                </div>
+
+                <div className={cx("video")}>
+                <video>
+                    <source src="./video/Download.mp4" type="video/mp4"/>
+                    </video>
+                </div>
+                <div className={cx("comment")}>
+                    <div  className={cx("select")}>
+                        <span className={cx("iconw")}>tim</span>
+                        <span className={cx("iconx")}>cmt</span>
+                        <span className={cx("icony")}>share</span>
+                        <span className={cx("iconz")}>luu</span>
+                    </div>
+                </div>
+           </div>
+
+        </div> 
+    )
 }
 
 export default PostItem;
